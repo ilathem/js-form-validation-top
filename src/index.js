0470c5b5ea1234541ce304b20f68e53f5f9ba101
ilathem <isaiahlathem@gmail.com>
2024-04-10 19:17:57 -0500

    Test and implement form

--- src/index.js
+++ src/index.js
@@ -1,24 +1,112 @@
-import "./index.css";
-import { getCode } from "country-list";
-import { postcodeValidator } from "postcode-validator";
+import './index.css';
+import { getCode, getName } from 'country-list';
+import { postcodeValidator } from 'postcode-validator';
+
+let data = {
+  email: '',
+  country: '',
+  countryCode: '',
+  zip: '',
+  password: '',
+  confirmPassword: '',
+};
 
 // https://stackoverflow.com/a/9204568
-const checkEmail = email => {
+const checkEmail = (email = '') => {
   const re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
- 
+  if (re.test(email)) {
+    document.body.querySelector('.email').setCustomValidity('');
+    data.email = email;
+  } else {
+    document.body
+      .querySelector('.email')
+      .setCustomValidity('Incorrect email format');
+  }
+};
+
 // https://www.npmjs.com/package/country-list
-const checkCountry = country => {
-  return getCode(country);
-}
+const checkCountry = (country = '') => {
+  const code = getCode(country);
+  const name = getName(country);
+  if (code || name) {
+    document.body.querySelector('.country').setCustomValidity('');
+    if (code) {
+      data.countryCode = code;
+      data.country = country;
+    } else if (name) {
+      data.countryCode = country;
+      data.country = name;
+    }
+  } else {
+    document.body
+      .querySelector('.country')
+      .setCustomValidity('Incorrect country name');
+  }
+};
 
 // https://www.npmjs.com/package/postcode-validator
-const checkZip = (zip, countryCode) => {
-  return postcodeValidator(zip, countryCode);
-}
+const checkZip = (zip = '') => {
+  let validate = false;
+  let errorMsg = 'Incorrect zip code';
+  try {
+    validate = postcodeValidator(zip, data.countryCode);
+  } catch (exception) {
+    errorMsg = exception.toString();
+  }
+  console.log(zip)
+  if (validate) {
+    document.body.querySelector('.zipCode').setCustomValidity('');
+    data.zip = zip;
+  } else {
+    document.body.querySelector('.zipCode').setCustomValidity(errorMsg);
+  }
+};
 
 const checkPasswords = (pass1, pass2) => {
-  return pass1 === pass2;
-}
+  if (pass1 === pass2 && pass1 !== '') {
+    document.body.querySelector('.password').setCustomValidity('');
+    document.body.querySelector('.confirmPassword').setCustomValidity('');
+    data.password = pass1;
+    data.confirmPassword = pass2;
+  } else {
+    document.body
+      .querySelector('.password')
+      .setCustomValidity("Passwords don't match");
+    document.body
+      .querySelector('.confirmPassword')
+      .setCustomValidity("Passwords don't match");
+  }
+};
 
+const addListeners = () => {
+  document.body.querySelector('.email').addEventListener('input', (e) => {
+    checkEmail(e.target.value);
+    e.target.reportValidity();
+  });
+  document.body.querySelector('.country').addEventListener('input', (e) => {
+    checkCountry(e.target.value);
+    e.target.reportValidity();
+  });
+  document.body.querySelector('.zipCode').addEventListener('input', (e) => {
+    checkZip(e.target.value, document.body.querySelector('.country').value);
+    e.target.reportValidity();
+  });
+  document.body
+    .querySelector('.confirmPassword')
+    .addEventListener('input', (e) => {
+      checkPasswords(
+        document.body.querySelector('.password').value || '',
+        e.target.value || ''
+      );
+      e.target.reportValidity();
+    });
+  document.body.querySelector('button').addEventListener('click', (e) => {
+    e.preventDefault();
+    document
+      .querySelectorAll('input')
+      .forEach((input) => input.reportValidity());
+    console.log(data);
+  });
+};
+
+addListeners();
